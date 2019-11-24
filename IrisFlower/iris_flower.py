dd750f878345e14f27699b764b653db5e19e86c7
Steven He <hez2010@outlook.com>
2019-11-24 15:36:47 +0800

    Fix typo

--- IrisFlower/iris_flower.py
+++ IrisFlower/iris_flower.py
@@ -2,10 +2,10 @@ from sklearn import datasets
 from sklearn import model_selection
 from sklearn import linear_model
 
-boston_house_data = datasets.load_iris()
-# print (boston_house_data)
+iris_flower = datasets.load_iris()
+# print (iris_flower)
 
-x_input, x_test, y_input, y_test = model_selection.train_test_split(boston_house_data['data'], boston_house_data['target'])
+x_input, x_test, y_input, y_test = model_selection.train_test_split(iris_flower['data'], iris_flower['target'])
 
 model = linear_model.LogisticRegression()
 model.fit(x_input, y_input)
