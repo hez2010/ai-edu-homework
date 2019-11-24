20f2575e1d4385a467c2465a06194f4f18b73101
Steven He <hez2010@outlook.com>
2019-11-24 15:34:00 +0800

    Add Boston House Price & Iris Flower

---
+++ BostonHousePrice/boston_house_price.py
@@ -0,0 +1,15 @@
+from sklearn import datasets
+from sklearn import model_selection
+from sklearn import linear_model
+
+boston_house_data = datasets.load_boston()
+# print (boston_house_data)
+
+x_input, x_test, y_input, y_test = model_selection.train_test_split(boston_house_data['data'], boston_house_data['target'])
+
+model = linear_model.LinearRegression()
+model.fit(x_input, y_input)
+score = model.score(x_test, y_test)
+predict = model.predict(x_test)
+
+print(score)
